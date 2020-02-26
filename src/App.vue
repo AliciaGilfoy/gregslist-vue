a1c7c3faf607502fd6506a72187db1e61e6d4cbd
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-02-26 14:26:54 -0700

    home vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container-fluid">
+    <div id="nav" class="text-center mt-2">
+      <router-link to="/">
+        <h1>
+          <i class="fas fa-peace mr-2"></i>GregsList
+        </h1>
+      </router-link>
+      <router-link class="ml-3" to="/cars">Cars</router-link>
+      <router-link class="ml-3" to="/jobs">Jobs</router-link>
+      <router-link class="ml-3" to="/houses">Houses</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<style scoped>
+i {
+  color: purple;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+  <div class="home container">
+    <div class="row mt-3">
+      <h1 class="col-12 text-center mt-3">Welcome to GregsList!</h1>
+      <h3
+        class="col-12 text-center mt-3 text-secondary"
+      >Here you can search for cars and houses to buy. We also have a new page just for jobs! You can list your items or jobs too!</h3>
+      <h5 class="col-12 text-center mt-3 text-dark">What would you like to list?</h5>
+    </div>
+    <div class="row mt-3 d-flex justify-content-center">
+      <button
+        @click="carForm = true, houseForm = false, jobForm = false"
+        class="btn btn-primary mx-2"
+      >Sell A Car</button>
+      <button
+        @click="carForm = false, houseForm = true, jobForm = false"
+        class="btn btn-primary mx-2"
+      >Sell A House</button>
+      <button
+        @click="carForm = false, houseForm = false, jobForm = true"
+        class="btn btn-primary mx-2"
+      >Post A Job</button>
+    </div>
+    <div class="row mt-3">
+      <create-car v-if="carForm" />
+      <create-house v-else-if="houseForm" />
+      <create-job v-else-if="jobForm" />
+      <div class="col-12 text-center" v-else>
+        <p>
+          <em>Check our stock by clicking cars, houses, or jobs at the top of the page.</em>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import CreateCar from "@/components/CreateCar";
+import CreateHouse from "@/components/CreateHouse";
+import CreateJob from "@/components/CreateJob";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      carForm: false,
+      houseForm: false,
+      jobForm: false
+    };
+  },
+  components: {
+    CreateCar,
+    CreateJob,
+    CreateHouse
+  }
+};
+</script>
